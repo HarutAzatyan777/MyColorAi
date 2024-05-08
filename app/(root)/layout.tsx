67676dc03f692687649ab0db3927ba4fd69cba10
sky <harutazatyan45@gmail.com>
2024-05-08 15:56:01 +0400

    08.05.2024

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Sidebar from "../../components/Shared/Sidebar";
+import MobileNav from "../../components/Shared/MobileNav";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
       <Sidebar />
+      <MobileNav />
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
