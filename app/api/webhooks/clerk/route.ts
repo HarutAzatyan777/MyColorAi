9ef06cd9096aaebd24fdc9b371b5b29cc8f04be3
HarutAzatyan777 <harutazatyan45@gmail.com>
2024-05-12 20:58:06 +0400

    10.05.2024-2

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,14 +1,15 @@
-import { Webhook } from 'svix'
-import { headers } from 'next/headers'
-import { WebhookEvent } from '@clerk/nextjs/server'
+import { Webhook } from 'svix';
+import { headers } from 'next/headers';
+import { WebhookEvent } from '@clerk/nextjs/server';
+import { createUser } from '../../../../lib/actions/user.actions'; // Import createUser function
+import { connect } from '../../../../lib/database/mongoose';
 
 export async function POST(req: Request) {
-
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
-  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
   if (!WEBHOOK_SECRET) {
-    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local')
+    throw new Error('Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local');
   }
 
   // Get the headers
@@ -21,17 +22,17 @@ export async function POST(req: Request) {
   if (!svix_id || !svix_timestamp || !svix_signature) {
     return new Response('Error occured -- no svix headers', {
       status: 400
-    })
+    });
   }
 
   // Get the body
-  const payload = await req.json()
+  const payload = await req.json();
   const body = JSON.stringify(payload);
 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt: WebhookEvent
+  let evt: WebhookEvent;
 
   // Verify the payload with the headers
   try {
@@ -39,20 +40,31 @@ export async function POST(req: Request) {
       "svix-id": svix_id,
       "svix-timestamp": svix_timestamp,
       "svix-signature": svix_signature,
-    }) as WebhookEvent
+    }) as WebhookEvent;
   } catch (err) {
     console.error('Error verifying webhook:', err);
     return new Response('Error occured', {
       status: 400
-    })
+    });
   }
 
   // Do something with the payload
   // For this guide, you simply log the payload to the console
   const { id } = evt.data;
   const eventType = evt.type;
-  console.log(`Webhook with and ID of ${id} and type of ${eventType}`)
-  console.log('Webhook body:', body)
+  console.log(`Webhook with an ID of ${id} and type of ${eventType}`);
+  console.log('Webhook body:', body);
 
-  return new Response('', { status: 200 })
+  // Create user using the payload
+  try {
+    await connect(); // Connect to the database
+    await createUser(payload); // Create the user
+  } catch (error) {
+    console.log(error);
+    return new Response('Error occurred while creating user', {
+      status: 500
+    });
+  }
+
+  return new Response('', { status: 200 });
 }
